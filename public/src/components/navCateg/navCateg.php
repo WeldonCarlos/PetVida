8ecd2cdcfc85da64d1ee8eb8eba2871c57a95596
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-04-22 16:36:29 -0300

    integração do componente de navegação das categorias

---
+++ public/src/components/navCateg/navCateg.php
@@ -0,0 +1,27 @@
+<nav>
+
+    <ul class="nav justify-content-start   "
+        style="background-color: #764ea0;  gap:30px ;  height: 50px; align-items: center; margin-top:40px; font-family: Ubuntu, sans-serif; font-weight: 500;
+        font-style: normal; font-size: 20px; ">
+        <li class="nav-item" style=" margin-left: 85px  ">
+            <a class="nav-link active" aria-current="page" href="#" style="color: white;">Cachorro</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: white;">Gato</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: white;">Aves e Passaros</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: white;">Coelhos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: white;">Peixes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#" style="color: white;">Répteis e Roedores</a>
+        </li>
+
+
+    </ul>
+</nav>
