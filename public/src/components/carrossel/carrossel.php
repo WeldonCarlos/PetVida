3c450ad66f256502988f3f1c693c13b8f300eedf
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-04-22 16:40:24 -0300

    integração das imagens no carrossel

--- public/src/components/carrossel/carrossel.php
+++ public/src/components/carrossel/carrossel.php
@@ -2,21 +2,21 @@
 
 
 
-<div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
+<div id="carouselExampleIndicators" class="carousel slide mx-auto" style="width: 86%; border-radius: 10px; overflow: hidden; margin-top: 30px;  " data-bs-ride="carousel">
+  <div class="carousel-indicators"  >
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+  <div class="carousel-inner" >
+    <div class="carousel-item active" >
+      <img src="./public/src/img/group_pet.jpg" class="d-block w-100  " alt="grupos de pets juntos">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="./public/src/img/love_pet.jpg" class="d-block w-100" alt="O dono carinhoso com o pet">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="./public/src/img/doctor_pet.jpg" class="d-block w-100" alt="doutora cuidando do pet">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
