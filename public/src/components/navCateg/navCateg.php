d2568373f387ec2ef0d524f7a94259834f7e4f12
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-04-24 10:02:11 -0300

    adicionando parametros de rotas

--- public/src/components/navCateg/navCateg.php
+++ public/src/components/navCateg/navCateg.php
@@ -4,16 +4,16 @@
         style="background-color: #764ea0;  gap:30px ;  height: 50px; align-items: center; margin-top:40px; font-family: Ubuntu, sans-serif; font-weight: 500;
         font-style: normal; font-size: 20px; ">
         <li class="nav-item" style=" margin-left: 85px  ">
-            <a class="nav-link active" aria-current="page" href="#" style="color: white;">Cachorro</a>
+            <a class="nav-link active" aria-current="page" href="index.php?page=dog" style="color: white;">Cachorro</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#" style="color: white;">Gato</a>
+            <a class="nav-link" href="index.php?page=cat" style="color: white;">Gato</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#" style="color: white;">Aves e Passaros</a>
+            <a class="nav-link" href="index.php?page=aves" style="color: white;">Aves e Passaros</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#" style="color: white;">Coelhos</a>
+            <a class="nav-link" href="index.php?page=coelhos" style="color: white;">Coelhos</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#" style="color: white;">Peixes</a>
