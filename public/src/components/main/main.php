8eeae1599294196b8422b4b84a0b01fbbe53ef99
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-04-24 10:01:53 -0300

    adicionando mais rotas

--- public/src/components/main/main.php
+++ public/src/components/main/main.php
@@ -1,25 +1,47 @@
-
 <?php
 
-include("./config/dbConfig.php");
+include ("./dbConfig.php");
 
 
-switch(@$_REQUEST['page']){
+switch (@$_REQUEST['page']) {
 
   case "home":
-    include("./app/view/home/home.php");
-    break; 
-    
+    include ("./app/view/home/home.php");
+    break;
+
   case "login":
-    include("./app/view/login/login.php");
-    break;  
+    include ("./app/view/login/login.php");
+    break;
+
+  case "cadastro":
+    include ("./app/view/cadastro/cadastro.php");
+    break;
+
+  case "salvar":
+    include ("./salvar-usuario.php");
+    break;
+
+  case "dog":
+    include ("./app/view/categorias/cachorros/cachorros.php");
+    break;
+
+  case "cat":
+    include ("./app/view/categorias/gatos/gatos.php");
+    break;
+
+  case "aves":
+    include ("./app/view/categorias/avesPassaros/aves.php");
+    break;
+
+  case "coelhos":
+    include ("./app/view/categorias/coelhos/coelhos.php");
+    break;
+
+
 
   default:
-    include("./app/view/home/home.php");
-    
-};
+    include ("./app/view/home/home.php");
 
+}
+;
 
-
-
-?>
