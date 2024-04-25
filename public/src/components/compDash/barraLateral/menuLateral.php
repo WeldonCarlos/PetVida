980e2e7563c4ff78b71cefa2b0fe321a978f9694
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-04-25 18:07:22 -0300

    integração dos links e icones do menus lateral

---
+++ public/src/components/compDash/barraLateral/menuLateral.php
@@ -0,0 +1,32 @@
+
+
+<link rel="stylesheet" href="../../../public/src/components/compDash/barraLateral/menuLateral.css">
+
+<section class="containerGeral">
+    <div class="container-Img-Logo">
+        <img src="../../../public/src/img/dash_painel_lateral/logoDash.png" alt=" logo da petVida">
+    </div>
+    <nav class="navegacaoLateral">
+
+
+        <ul class="lista-nav" >
+            
+    
+            <li class="opcoes-nav">
+                <img src="../../../public/src/img/dash_painel_lateral/tosa-de-animais.png" alt=" logo da petVida">
+                <a href="dashboard.php?dashpage=tosa">Agendar banho e tosa</a>
+            </li>
+            <li class="opcoes-nav">
+                <img src="../../../public/src/img/dash_painel_lateral/doctor-dog.png" alt=" logo da petVida">
+                <a href="dashboard.php?dashpage=consulta">Agendar consulta</a>
+            </li>
+
+            <li class="opcoes-nav">
+
+                <img src="../../../public/src/img/dash_painel_lateral/hotel-de-animais.png" alt=" logo da petVida">
+                <a href="dashboard.php?dashpage=hotelzinho">Agendar hotelzinho</a>
+                
+            </li>
+        </ul>
+    </nav>
+</section>
