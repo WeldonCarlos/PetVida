697d5bfca3c10a3e23e60029047a1d68a435565a
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-05-08 09:55:43 -0300

    Alteração do modelo de estilo

--- public/src/components/compDash/barraLateral/menuLateral.php
+++ public/src/components/compDash/barraLateral/menuLateral.php
@@ -1,6 +1,88 @@
 
+<style>
+    
+.containerGeral{
+    display:flex; 
+    width: 22%;
+    height: 100vh;
+    background-color: #854bc4;
+    flex-direction: column;
+    gap: 40px;
+}
 
-<link rel="stylesheet" href="../../../public/src/components/compDash/barraLateral/menuLateral.css">
+.navegacaoLateral{
+    display: flex;
+    padding-left: 12px;
+   
+
+}
+
+
+.container-Img-Logo{
+    margin: 8px 0 0 17px;
+}
+
+.navegacaoLateral li{
+   
+ list-style-type: none;
+     
+}
+
+
+
+
+.navegacaoLateral a{
+    text-decoration: none;
+    color: white;
+    width: 100%;
+    flex-wrap: nowrap;
+    font-size: 14px;
+    font-family: "Ubuntu", sans-serif;
+    font-weight: 600;
+    margin-top:12px;
+    transition: color 0.3s ease;
+
+     
+   
+}
+
+.navegacaoLateral a:hover{
+   
+    color: #73d000;
+     
+   
+}
+
+
+.lista-nav{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    gap: 20px;
+   
+}
+
+.opcoes-nav{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    width: 100%;
+    gap: 13px;
+}
+
+.opcoes-nav img{
+    width: 35px;
+   
+   
+}
+
+</style>
+
+
+
 
 <section class="containerGeral">
     <div class="container-Img-Logo">
--- public/src/components/compDash/barraTopo/barraTopo.php
+++ public/src/components/compDash/barraTopo/barraTopo.php
@@ -1,4 +1,49 @@
-<link rel="stylesheet" href="../../../public/src/components/compDash/barraTopo/barraTopo.css">
+<style>
+    .navTopo{
+    display: flex;
+    justify-content: flex-end;
+    height: 55px;
+    width: 100%;
+    background-color: white;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); /* Sombra */
+    align-items: center;
+   
+    
+}
+
+.nav-content{
+    display: flex;
+    padding-right: 20px;
+    gap: 20px;
+    align-items: center;
+}
+
+#ancoraSair{
+    text-decoration: none;
+    background-color: #854bc4;
+    color: white;
+    padding: 6px 18px;
+    border-radius: 6px;
+   
+    transition: background-color 0.3s ease;
+    font-family: "Ubuntu", sans-serif;
+    font-weight: 600;
+}
+
+.bem-Vindo{
+    font-size: 18px;
+    font-family: "Ubuntu", sans-serif;
+    color: #854bc4;
+    font-weight: 500;
+}
+
+#ancoraSair:hover {
+    
+    background-color: #61368f;
+     
+}
+</style>
+
 
 <header class="navTopo">
     <nav class="nav">
