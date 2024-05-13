d6e7b1eb074b5f6a76fef0be2d8bc4d082b5255d
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-05-13 11:03:24 -0300

    ..

---
+++ produtos/produtosCoelhos.php
@@ -0,0 +1,75 @@
+<?php
+
+// Array de produtos
+$produtosCoelhos = array(
+    'coelhos' => array(
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/AlimentadorFeno.jpg',
+            'produto' => 'Alimentador',
+            'descrição' => 'Ração para Gatos',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/almofadaHamster.jpg',
+            'produto' => 'Almofada G',
+            'preco' => 15.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/AlmofadaoGuine.jpg',
+            'produto' => 'Almofadão GG',
+            'descrição' => 'Pestisco refil',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/CaixaAreiaCoelho.jpg',
+            'produto' => 'Arranhador top',
+            'preco' => 15.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/CapaGaiolaCoelho.jpg',
+            'produto' => 'Capa M',
+            'descrição' => 'Ração para Cães',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/ColeiraCoelho.jpg',
+            'produto' => 'Coleira T/P',
+            'preco' => 15.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/DispensadorAguaComida.jpg',
+            'produto' => 'Dispensador ',
+            'descrição' => 'Caixa de areia inteligente',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/GaiolaPorquinhoIndia.jpg',
+            'produto' => 'Gaiola T/120mm',
+            'preco' => 15.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/GaiolaRamster.jpg',
+            'produto' => 'Gaiola PREMIUM',
+            'descrição' => 'Torre de bolinhas premium',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/MegazoRacao.jpg',
+            'produto' => 'Ração PREMIUM',
+            'preco' => 15.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/pecasBrinquedos.jpg',
+            'produto' => 'Kit Brinquedos',
+            'descrição' => 'Ração para Cães',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/coelhos/RacaoCoelho.jpg',
+            'produto' => 'Ração TOP',
+            'preco' => 15.49
+        ),
+        
+    )
+  
+);
