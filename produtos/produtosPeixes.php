8600f8f18cbc262aba8348d5a637c2fde15b9c33
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-05-13 11:02:39 -0300

    ..

---
+++ produtos/produtosPeixes.php
@@ -0,0 +1,75 @@
+<?php
+
+// Array de produtos
+$produtosPeixes = array(
+    'peixes' => array(
+        array(
+            'imagem' => './produtos/img-produtos/peixes/AcessoriosParaAquario.jpg',
+            'produto' => 'Acessorio p/aquario',
+            'descrição' => 'Ração para Gatos',
+            'preco' => 21.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/AlimentadorAutomaticoPeixe.jpg',
+            'produto' => 'Alimentador aut.',
+            'preco' => 499,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/antParazitaPeixe.jpg',
+            'produto' => 'Anti-Parazita 150ml',
+            'descrição' => '',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/aquario3Em1CFiltro.jpg',
+            'produto' => 'Arranhador top',
+            'preco' => 299,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/BombaSubmersaAgua.jpg',
+            'produto' => 'Bomba submersa',
+            'descrição' => '',
+            'preco' => 151,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/EnfeiteAquarioBobEsponja.jpg',
+            'produto' => 'Esfeite Bob-Esponja',
+            'preco' => 15.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/FiltroTanquePeixes.jpg',
+            'produto' => 'Filtro Premium ',
+            'descrição' => '',
+            'preco' => 79,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/LamaAquarioPeixes.jpg',
+            'produto' => 'Lama p/Aquario',
+            'preco' => 30.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/lampadaBolhasMulticolorida.jpg',
+            'produto' => 'Lampada Colorida',
+            'descrição' => '',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/racaoPeixes.jpg',
+            'produto' => 'Ração PREMIUM',
+            'preco' => 15.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/racaoPeixesPequenos.jpg',
+            'produto' => 'Ração p/pequeno',
+            'descrição' => 'Ração para Cães',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/peixes/AquarioPequenoAcrilico.jpg',
+            'produto' => 'Aquario T/P',
+            'preco' => 15.49
+        ),
+        
+    )
+  
+);
---
+++ produtos/produtosRepteis.php
@@ -0,0 +1,75 @@
+<?php
+
+// Array de produtos
+$produtosRepteis = array(
+    'repteis' => array(
+        array(
+            'imagem' => './produtos/img-produtos/repteis/AlconRacaoRepteis.jpg',
+            'produto' => 'Ração p/Repteis',
+            'descrição' => '',
+            'preco' => 21.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/AlimentadorComidaRepteis.jpg',
+            'produto' => 'Alimentador ',
+            'preco' => 499,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/AlmofadaAquecimentoRepteis.jpg',
+            'produto' => 'Almofada de Aquecimento',
+            'descrição' => '',
+            'preco' => 299,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/DecoracaoTerrarioRepteis.jpg',
+            'produto' => 'Decoração Terrario',
+            'preco' => 88,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/FerramentaCapturaRepteis.jpg',
+            'produto' => 'Vara de captura',
+            'descrição' => '',
+            'preco' => 151,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/GaiolaMagneticaAcrilico.jpg',
+            'produto' => 'Gaiola Magnetica',
+            'preco' => 800.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/KitApanhadorInsetos.jpg',
+            'produto' => 'Armadilha Apanhador',
+            'descrição' => '',
+            'preco' => 79,99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/LampadaCalorRepteis.jpg',
+            'produto' => 'Lâmpada de calor',
+            'preco' => 30.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/OrnamentoCavernaTerario.jpg',
+            'produto' => 'Ornamento',
+            'descrição' => '',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/PratoAlimentacaoTartaruga.jpg',
+            'produto' => 'Prato alimentação',
+            'preco' => 15.49
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/TerrariosRepteis.jpg',
+            'produto' => 'Terrario',
+            'descrição' => '',
+            'preco' => 10.99
+        ),
+        array(
+            'imagem' => './produtos/img-produtos/repteis/TigelasGrandesAlimentacao.jpg',
+            'produto' => 'Tigela GG',
+            'preco' => 15.49
+        ),
+        
+    )
+  
+);
